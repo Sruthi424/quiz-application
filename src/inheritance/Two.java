540003e2f339928ef94348de8af26f0a755c374d
shank <shank@Sruthi_C>
2024-12-20 12:14:18 +0530

    initial push

---
+++ src/inheritance/Two.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Two extends one {
+public void print_world()
+{
+	System.out.println("world");
+}
+	
+	}
+
---
+++ src/inheritance/start.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class start {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Two t=new Two();
+t.print_hello();
+t.print_world();
+t.print_hello_world();
+t.print_world();
+	}
+
+}
